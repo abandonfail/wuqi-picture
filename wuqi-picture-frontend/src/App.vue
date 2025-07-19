09a915929d637d7df5e2628f08ecd514ff652f24
abandon <3240529156@qq.com>
2025-07-19 14:45:37 +0800

    提交后端和前端代码

---
+++ wuqi-picture-frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <BasicLayout />
+  </div>
+</template>
+
+
+<script setup lang="ts">
+import { healthUsingGet } from './api/mainController';
+import BasicLayout from './layouts/BasicLayout.vue';
+import { useLoginUserStore } from './stores/useLoginUserStore';
+
+const loginUserStore = useLoginUserStore()
+loginUserStore.fetchLoginUser()
+
+healthUsingGet().then((res) => {
+  console.log(res)
+})
+
+</script>
+
+
+
+<style scoped>
+
+</style>
