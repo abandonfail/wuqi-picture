834a79d55fd3667b9881750bf441f683934285d1
abandon <3240529156@qq.com>
2025-07-20 17:10:39 +0800

    用户模块

---
+++ src/main/java/com/wuqipicturebackend/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.wuqipicturebackend.mapper;
+
+import com.wuqipicturebackend.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 陈
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2025-07-19 15:30:32
+* @Entity com.wuqipicturebackend.model.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
